7ada0037f246693fb32a3fadaf9ecedf508b1322
Devin Herath <devdhera@gmail.com>
2018-12-27 23:56:19 +0530

    Semantic Playground

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import { Button } from 'semantic-ui-react'
 
 class App extends Component {
   render() {
     return (
       <div>
         <h1>Event Monster</h1>
+        <button className="ui icon button">
+          <i className="smile icon"></i>
+          CSS Button
+        </button>
+        <Button icon="smile" content="React Button" />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'semantic-ui-css/semantic.min.css';
 import './index.css';
 import App from './app/layout/App';
 import * as serviceWorker from './serviceWorker';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
