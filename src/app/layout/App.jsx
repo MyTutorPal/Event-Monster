51e29488b3a07ccb0dde10e5dcc1a9058486d0d6
Devin Herath <devdhera@gmail.com>
2018-12-28 23:54:51 +0530

    Removed unused imports

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Button } from 'semantic-ui-react';
 import EventDashboard from '../../features/event/EventDashboard/EventDashboard'
 
 class App extends Component {
