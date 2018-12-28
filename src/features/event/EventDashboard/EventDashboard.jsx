27fda5e27984e4a3d115f8eecc660080f37bb4fd
Devin Herath <devdhera@gmail.com>
2018-12-29 00:40:39 +0530

    Added Event List to Dashboard

--- src/features/event/EventDashboard/EventDashboard.jsx
+++ src/features/event/EventDashboard/EventDashboard.jsx
@@ -1,12 +1,13 @@
-import React, { Component } from 'react'
-import { Grid } from 'semantic-ui-react'
+import React, { Component } from 'react';
+import { Grid } from 'semantic-ui-react';
+import EventList from '../EventList/EventList'
 
 class EventDashboard extends Component {
     render() {
         return (
             <Grid>
                 <Grid.Column width={10}>
-                    <h2>Left Column</h2>
+                    <EventList />
                 </Grid.Column>
                 <Grid.Column width={6}>
                     <h2>Right Column</h2>
