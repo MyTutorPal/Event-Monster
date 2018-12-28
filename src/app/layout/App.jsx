cdc7442ff75fb7f97e6e0354908a73dc88f9033c
Devin Herath <devdhera@gmail.com>
2018-12-29 00:15:00 +0530

    Refactoring to R16.2

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
-import EventDashboard from '../../features/event/EventDashboard/EventDashboard'
+import { Container } from 'semantic-ui-react'
+import EventDashboard from '../../features/event/EventDashboard/EventDashboard';
+import NavBar from '../../features/nav/NavBar/NavBar'
 
 class App extends Component {
   render() {
-    return (
-      <div>
-        <h1>Event Monster</h1>
+    return [
+      <NavBar />,
+      <Container className="main">
         <EventDashboard />
-      </div>
-    );
+      </Container>
+    ];
   }
 }
 
