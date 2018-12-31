86444b17d17247c00829406e178a0d7c81099e26
Devin Herath <devdhera@gmail.com>
2019-01-01 00:43:20 +0530

    :rocket: Passing down static props to components

--- src/features/event/EventList/EventListAttendee.jsx
+++ src/features/event/EventList/EventListAttendee.jsx
@@ -3,9 +3,10 @@ import { List, Image } from 'semantic-ui-react'
 
 class EventListAttendee extends Component {
     render() {
+        const { attendee } = this.props;
         return (
             <List.Item>
-                <Image as='a' size='mini' circular src="https://randomuser.me/api/portraits/women/42.jpg" />
+                <Image as='a' size='mini' circular src={attendee.photoURL} />
             </List.Item>
         )
     }
