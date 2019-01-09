94eda9e60b9c07e03fe930c9fb7148b0638dcf89
Devin Herath <devdhera@gmail.com>
2019-01-09 20:09:20 +0530

    Added Form Button Functionalities

--- src/features/event/EventDashboard/EventDashboard.jsx
+++ src/features/event/EventDashboard/EventDashboard.jsx
@@ -62,7 +62,23 @@ class EventDashboard extends Component {
             events: eventsDashboard,
             isOpen: false
         }
+
+        this.handleFormOpen = this.handleFormOpen.bind(this);
+        this.handleCancel = this.handleCancel.bind(this);
     }
+
+    handleFormOpen() {
+        this.setState({
+            isOpen: true
+        })
+    }
+
+    handleCancel() {
+        this.setState({
+            isOpen: false
+        })
+    }
+
     render() {
         return (
             <Grid>
@@ -70,9 +86,9 @@ class EventDashboard extends Component {
                     <EventList events={this.state.events} />
                 </Grid.Column>
                 <Grid.Column width={6}>
-                    <Button positive content="Create Event" />
+                    <Button onClick={this.handleFormOpen} positive content="Create Event" />
                     {this.state.isOpen &&
-                        <EventForm />}
+                        <EventForm handleCancel={this.handleCancel} />}
                 </Grid.Column>
             </Grid>
         )
--- src/features/event/EventForm/EventForm.jsx
+++ src/features/event/EventForm/EventForm.jsx
@@ -3,6 +3,7 @@ import { Segment, Form, Button } from 'semantic-ui-react'
 
 class EventForm extends Component {
     render() {
+        const { handleCancel } = this.props;
         return (
             <Segment>
                 <Form>
@@ -29,7 +30,7 @@ class EventForm extends Component {
                     <Button positive type="submit">
                         Submit
                 </Button>
-                    <Button type="button">Cancel</Button>
+                    <Button onClick={handleCancel} type="button">Cancel</Button>
                 </Form>
             </Segment>
         )
