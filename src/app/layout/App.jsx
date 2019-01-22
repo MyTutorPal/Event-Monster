d81260c91dc86ccaa45082382c01d57e30570765
Devin Herath <devdhera@gmail.com>
2019-01-22 19:58:31 +0530

    Fixed #6 by changing the render() func

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
-import { Container } from 'semantic-ui-react'
+import { Container } from 'semantic-ui-react';
 import EventDashboard from '../../features/event/EventDashboard/EventDashboard';
-import NavBar from '../../features/nav/NavBar/NavBar'
+import NavBar from '../../features/nav/NavBar/NavBar';
 
 class App extends Component {
   render() {
-    return [
-      <NavBar />,
-      <Container className="main">
-        <EventDashboard />
-      </Container>
-    ];
+    return (
+      <div>
+        <NavBar />
+        <Container className="main">
+          <EventDashboard />
+        </Container>
+      </div>
+    );
   }
 }
 
