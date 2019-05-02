818df9b42be684204f39568bf17d973e15d7bb36
Devin <devdhera@gmail.com>
2019-05-02 11:39:26 +0530

    Configured Base Routing

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
 import { Container } from 'semantic-ui-react';
+import { Route } from 'react-router-dom';
 import EventDashboard from '../../features/event/EventDashboard/EventDashboard';
 import NavBar from '../../features/nav/NavBar/NavBar';
+import EventForm from '../../features/event/EventForm/EventForm';
+import SettingsDashboard from '../../features/user/Settings/SettingsDashboard';
+import UserDetailedPage from '../../features/user/UserDetailed/UserDetailedPage';
+import PeopleDashboard from '../../features/user/PeopleDashboard/PeopleDashboard';
+import EventDetailedPage from '../../features/event/EventDetailed/EventDetailedPage';
+import HomePage from '../../features/home/HomePage';
 
 class App extends Component {
   render() {
@@ -9,7 +16,13 @@ class App extends Component {
       <div>
         <NavBar />
         <Container className="main">
-          <EventDashboard />
+          <Route exact path="/" component={HomePage} />
+          <Route path="/events" component={EventDashboard} />
+          <Route path="/event/:id" component={EventDetailedPage} />
+          <Route path="/people" component={PeopleDashboard} />
+          <Route path="/profile/:id" component={UserDetailedPage} />
+          <Route path="/settings" component={SettingsDashboard} />
+          <Route path="/createEvent" component={EventForm} />
         </Container>
       </div>
     );
--- src/features/nav/NavBar/NavBar.jsx
+++ src/features/nav/NavBar/NavBar.jsx
@@ -1,27 +1,41 @@
 import React, { Component } from 'react';
-import { Menu, Container, Button } from 'semantic-ui-react'
+import { Menu, Container, Button } from 'semantic-ui-react';
+import { NavLink, Link } from 'react-router-dom';
 
 class NavBar extends Component {
-    render() {
-        return (
-            <Menu inverted fixed="top">
-                <Container>
-                    <Menu.Item header>
-                        <img src="assets/logo.png" alt="logo" />
-                        Event Monster
-            </Menu.Item>
-                    <Menu.Item name="Events" />
-                    <Menu.Item>
-                        <Button floated="right" positive inverted content="Create Event" />
-                    </Menu.Item>
-                    <Menu.Item position="right">
-                        <Button basic inverted content="Login" />
-                        <Button basic inverted content="Sign Out" style={{ marginLeft: '0.5em' }} />
-                    </Menu.Item>
-                </Container>
-            </Menu>
-        )
-    }
+  render() {
+    return (
+      <Menu inverted fixed="top">
+        <Container>
+          <Menu.Item as={Link} to="/" header>
+            <img src="assets/logo.png" alt="logo" />
+            Event Monster
+          </Menu.Item>
+          <Menu.Item as={NavLink} to="/events" name="Events" />
+          <Menu.Item as={NavLink} to="/people" name="People" />
+          <Menu.Item>
+            <Button
+              as={Link}
+              to="/createEvent"
+              floated="right"
+              positive
+              inverted
+              content="Create Event"
+            />
+          </Menu.Item>
+          <Menu.Item position="right">
+            <Button basic inverted content="Login" />
+            <Button
+              basic
+              inverted
+              content="Sign Out"
+              style={{ marginLeft: '0.5em' }}
+            />
+          </Menu.Item>
+        </Container>
+      </Menu>
+    );
+  }
 }
 
-export default NavBar
+export default NavBar;
