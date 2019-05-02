d507fb2c88d384c426f13922498b3fa9f70971c8
Devin <devdhera@gmail.com>
2019-05-02 18:58:25 +0530

    Connected event actions to event form and refactor

--- src/features/event/EventDashboard/EventDashboard.jsx
+++ src/features/event/EventDashboard/EventDashboard.jsx
@@ -1,94 +1,29 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Grid, Button } from 'semantic-ui-react';
-import cuid from 'cuid';
+import { Grid } from 'semantic-ui-react';
 import EventList from '../EventList/EventList';
-import EventForm from '../EventForm/EventForm';
-import { createEvent, deleteEvent, updateEvent } from '../eventActions';
+import { deleteEvent } from '../eventActions';
 
 const mapState = state => ({
   events: state.events
 });
 
 const actions = {
-  createEvent,
-  deleteEvent,
-  updateEvent
+  deleteEvent
 };
 class EventDashboard extends Component {
-  state = {
-    isOpen: false,
-    selectedEvent: null
-  };
-
-  handleFormOpen = () => {
-    this.setState({
-      selectedEvent: null,
-      isOpen: true
-    });
-  };
-
-  handleCancel = () => {
-    this.setState({
-      isOpen: false
-    });
-  };
-
-  handleUpdateEvent = updatedEvent => {
-    this.props.updateEvent(updatedEvent);
-    this.setState({
-      isOpen: false,
-      selectedEvent: null
-    });
-  };
-
-  handleOpenEvent = eventToOpen => () => {
-    this.setState({
-      selectedEvent: eventToOpen,
-      isOpen: true
-    });
-  };
-
-  handleCreateEvent = newEvent => {
-    newEvent.id = cuid();
-    newEvent.hostPhotoURL = '/assets/user.png';
-    this.props.createEvent(newEvent);
-    this.setState({
-      isOpen: false
-    });
-  };
-
   handleDeleteEvent = eventId => () => {
     this.props.deleteEvent(eventId);
   };
 
   render() {
-    const { selectedEvent } = this.state;
     const { events } = this.props;
     return (
       <Grid>
         <Grid.Column width={10}>
-          <EventList
-            deleteEvent={this.handleDeleteEvent}
-            onEventOpen={this.handleOpenEvent}
-            events={events}
-          />
+          <EventList deleteEvent={this.handleDeleteEvent} events={events} />
         </Grid.Column>
-        <Grid.Column width={6}>
-          <Button
-            onClick={this.handleFormOpen}
-            positive
-            content="Create Event"
-          />
-          {this.state.isOpen && (
-            <EventForm
-              updateEvent={this.handleUpdateEvent}
-              selectedEvent={selectedEvent}
-              createEvent={this.handleCreateEvent}
-              handleCancel={this.handleCancel}
-            />
-          )}
-        </Grid.Column>
+        <Grid.Column width={6} />
       </Grid>
     );
   }
--- src/features/event/EventForm/EventForm.jsx
+++ src/features/event/EventForm/EventForm.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import cuid from 'cuid';
 import { Segment, Form, Button } from 'semantic-ui-react';
+import { createEvent, updateEvent } from '../eventActions';
 
 const mapState = (state, ownProps) => {
   const eventId = ownProps.match.params.id;
@@ -21,6 +23,11 @@ const mapState = (state, ownProps) => {
     event
   };
 };
+
+const actions = {
+  createEvent,
+  updateEvent
+};
 class EventForm extends Component {
   state = {
     event: Object.assign({}, this.props.event)
@@ -30,8 +37,15 @@ class EventForm extends Component {
     evt.preventDefault();
     if (this.state.event.id) {
       this.props.updateEvent(this.state.event);
+      this.props.history.goBack();
     } else {
-      this.props.createEvent(this.state.event);
+      const newEvent = {
+        ...this.state.event,
+        id: cuid(),
+        hostPhotoURL: '/assets/user.png'
+      };
+      this.props.createEvent(newEvent);
+      this.props.history.push('/events');
     }
   };
 
@@ -98,7 +112,7 @@ class EventForm extends Component {
           <Button positive type="submit">
             Submit
           </Button>
-          <Button onClick={handleCancel} type="button">
+          <Button onClick={this.props.history.goBack} type="button">
             Cancel
           </Button>
         </Form>
@@ -107,4 +121,7 @@ class EventForm extends Component {
   }
 }
 
-export default connect(mapState)(EventForm);
+export default connect(
+  mapState,
+  actions
+)(EventForm);
