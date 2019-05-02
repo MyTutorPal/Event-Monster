93e0b6552c00dbc0b533959c6aeefa1f3355745f
Devin <devdhera@gmail.com>
2019-05-02 15:14:56 +0530

    Added Test Playgroud

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -9,6 +9,7 @@ import UserDetailedPage from '../../features/user/UserDetailed/UserDetailedPage'
 import PeopleDashboard from '../../features/user/PeopleDashboard/PeopleDashboard';
 import EventDetailedPage from '../../features/event/EventDetailed/EventDetailedPage';
 import HomePage from '../../features/home/HomePage';
+import TestComponent from '../../features/testarea/TestComponent';
 
 class App extends Component {
   render() {
@@ -26,6 +27,7 @@ class App extends Component {
               <Container className="main">
                 <Switch>
                   <Route path="/events" component={EventDashboard} />
+                  <Route path="/test" component={TestComponent} />
                   <Route path="/event/:id" component={EventDetailedPage} />
                   <Route path="/people" component={PeopleDashboard} />
                   <Route path="/profile/:id" component={UserDetailedPage} />
---
+++ src/features/testarea/TestComponent.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class TestComponent extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Test Area</h1>
+      </div>
+    );
+  }
+}
+
+export default TestComponent;
