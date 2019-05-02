f1bc33bc4f23058f9311ed213d5d805e648671d9
Devin <devdhera@gmail.com>
2019-05-02 11:52:29 +0530

    Configured Homepage

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Container } from 'semantic-ui-react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import EventDashboard from '../../features/event/EventDashboard/EventDashboard';
 import NavBar from '../../features/nav/NavBar/NavBar';
 import EventForm from '../../features/event/EventForm/EventForm';
@@ -14,16 +14,28 @@ class App extends Component {
   render() {
     return (
       <div>
-        <NavBar />
-        <Container className="main">
+        <Switch>
           <Route exact path="/" component={HomePage} />
-          <Route path="/events" component={EventDashboard} />
-          <Route path="/event/:id" component={EventDetailedPage} />
-          <Route path="/people" component={PeopleDashboard} />
-          <Route path="/profile/:id" component={UserDetailedPage} />
-          <Route path="/settings" component={SettingsDashboard} />
-          <Route path="/createEvent" component={EventForm} />
-        </Container>
+        </Switch>
+
+        <Route
+          path="/(.+)"
+          render={() => (
+            <div>
+              <NavBar />
+              <Container className="main">
+                <Switch>
+                  <Route path="/events" component={EventDashboard} />
+                  <Route path="/event/:id" component={EventDetailedPage} />
+                  <Route path="/people" component={PeopleDashboard} />
+                  <Route path="/profile/:id" component={UserDetailedPage} />
+                  <Route path="/settings" component={SettingsDashboard} />
+                  <Route path="/createEvent" component={EventForm} />
+                </Switch>
+              </Container>
+            </div>
+          )}
+        />
       </div>
     );
   }
--- src/features/home/HomePage.jsx
+++ src/features/home/HomePage.jsx
@@ -1,9 +1,45 @@
 import React from 'react';
 
-const HomePage = () => {
+const HomePage = ({ history }) => {
   return (
     <div>
-      <h1>Home Page</h1>
+      <div className="ui inverted vertical masthead center aligned segment">
+        <div className="ui text container">
+          <h1 className="ui inverted stackable header">
+            <img
+              className="ui image massive"
+              src="/assets/logo.png"
+              alt="logo"
+            />
+            <div className="content">Event Monster</div>
+          </h1>
+          <h2>Do whatever you want to do</h2>
+          <div
+            onClick={() => history.push('/events')}
+            className="ui huge white inverted button"
+          >
+            Get Started
+            <i className="right arrow icon" />
+          </div>
+        </div>
+      </div>
+      <div style={{ textAlign: 'center' }}>
+        Icons made by{' '}
+        <a href="http://www.freepik.com" title="Freepik">
+          Freepik
+        </a>{' '}
+        from{' '}
+        <a href="https://www.flaticon.com/" title="Flaticon">
+          www.flaticon.com
+        </a>{' '}
+        is licensed by{' '}
+        <a
+          href="http://creativecommons.org/licenses/by/3.0/"
+          title="Creative Commons BY 3.0"
+        >
+          CC 3.0 BY
+        </a>
+      </div>
     </div>
   );
 };
