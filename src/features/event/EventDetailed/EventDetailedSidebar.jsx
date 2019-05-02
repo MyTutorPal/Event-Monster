725523d5f8985925842f0441eaf07f3a7f3d8bf7
Devin <devdhera@gmail.com>
2019-05-02 17:47:08 +0530

    Base Mockups for sidebars, chats and infos

--- src/features/event/EventDetailed/EventDetailedSidebar.jsx
+++ src/features/event/EventDetailed/EventDetailedSidebar.jsx
@@ -1,9 +1,38 @@
 import React from 'react';
+import { Segment, List, Item, Label } from 'semantic-ui-react';
 
 const EventDetailedSidebar = () => {
   return (
     <div>
-      <h1>Sidebar</h1>
+      <Segment
+        textAlign="center"
+        style={{ border: 'none' }}
+        attached="top"
+        secondary
+        inverted
+        color="teal"
+      >
+        2 People Going
+      </Segment>
+      <Segment attached>
+        <List relaxed divided>
+          <Item style={{ position: 'relative' }}>
+            <Label
+              style={{ position: 'absolute' }}
+              color="orange"
+              ribbon="right"
+            >
+              Host
+            </Label>
+            <Item.Image size="tiny" src="/assets/user.png" />
+            <Item.Content verticalAlign="middle">
+              <Item.Header as="h3">
+                <a href={'/'}>Attendee Name</a>
+              </Item.Header>
+            </Item.Content>
+          </Item>
+        </List>
+      </Segment>
     </div>
   );
 };
