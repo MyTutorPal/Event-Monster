97879f8b35b7a3439809926f4238951e1fc4c238
Devin <devdhera@gmail.com>
2019-05-03 17:05:02 +0530

    Configured modal reducers

--- src/app/reducers/rootReducer.js
+++ src/app/reducers/rootReducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import { reducer as FormReducer } from 'redux-form';
 import testReducer from '../../features/testarea/testReducer';
 import eventReducer from '../../features/event/eventReducer';
+import modalReducer from '../../features/modals/modalReducer';
 
 const rootReducer = combineReducers({
   form: FormReducer,
   test: testReducer,
-  events: eventReducer
+  events: eventReducer,
+  modals: modalReducer
 });
 
 export default rootReducer;
