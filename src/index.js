5b87c48525a83114c54877ba55b09aabf2efa5d2
Devin <devdhera@gmail.com>
2019-05-03 22:04:29 +0530

    Added Firestore event listner

--- src/index.js
+++ src/index.js
@@ -10,10 +10,8 @@ import App from './app/layout/App';
 import * as serviceWorker from './serviceWorker';
 import { configureStore } from './app/store/configureStore';
 import ScrollToTop from './app/common/util/ScrollToTop';
-import { loadEvents } from './features/event/eventActions';
 
 const store = configureStore();
-store.dispatch(loadEvents());
 
 ReactDOM.render(
   <Provider store={store}>
