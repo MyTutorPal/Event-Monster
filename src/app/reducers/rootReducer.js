fa2da1050a2d9e842904d28b5b45089bce58dc1d
Devin <devdhera@gmail.com>
2019-05-03 18:11:55 +0530

    Configured Auth Reducers

--- src/app/reducers/rootReducer.js
+++ src/app/reducers/rootReducer.js
@@ -3,12 +3,14 @@ import { reducer as FormReducer } from 'redux-form';
 import testReducer from '../../features/testarea/testReducer';
 import eventReducer from '../../features/event/eventReducer';
 import modalReducer from '../../features/modals/modalReducer';
+import authReducer from '../../features/auth/authReducer';
 
 const rootReducer = combineReducers({
   form: FormReducer,
   test: testReducer,
   events: eventReducer,
-  modals: modalReducer
+  modals: modalReducer,
+  auth: authReducer
 });
 
 export default rootReducer;
