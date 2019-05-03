680e68dd8f4907020c13a4d2d7c13dbcbdaeedde
Devin <devdhera@gmail.com>
2019-05-03 20:20:57 +0530

    Base setup for Event Activity Component

--- src/features/event/EventDashboard/EventDashboard.jsx
+++ src/features/event/EventDashboard/EventDashboard.jsx
@@ -4,6 +4,7 @@ import { Grid } from 'semantic-ui-react';
 import EventList from '../EventList/EventList';
 import { deleteEvent } from '../eventActions';
 import LoadingComponent from '../../../app/layout/LoadingComponent';
+import EventActivity from '../EventActivity/EventActivity';
 
 const mapState = state => ({
   events: state.events,
@@ -26,7 +27,9 @@ class EventDashboard extends Component {
         <Grid.Column width={10}>
           <EventList deleteEvent={this.handleDeleteEvent} events={events} />
         </Grid.Column>
-        <Grid.Column width={6} />
+        <Grid.Column width={6}>
+          <EventActivity />
+        </Grid.Column>
       </Grid>
     );
   }
