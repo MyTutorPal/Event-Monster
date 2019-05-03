315e8dd53643db1825969608c768ce6777041f3d
Devin <devdhera@gmail.com>
2019-05-03 18:24:53 +0530

    Added Basic Mock Auth Flow

--- src/features/nav/NavBar/NavBar.jsx
+++ src/features/nav/NavBar/NavBar.jsx
@@ -5,16 +5,18 @@ import { NavLink, Link, withRouter } from 'react-router-dom';
 import SignedOutMenu from '../Menus/SignedOutMenu';
 import SignedInMenu from '../Menus/SignedInMenu';
 import { openModal } from '../../modals/modalActions';
+import { logout } from '../../auth/authActions';
 
 const actions = {
-  openModal
+  openModal,
+  logout
 };
 
-class NavBar extends Component {
-  state = {
-    authenticated: false
-  };
+const mapState = state => ({
+  auth: state.auth
+});
 
+class NavBar extends Component {
   handleSignIn = () => {
     this.props.openModal('LoginModal');
   };
@@ -24,14 +26,13 @@ class NavBar extends Component {
   };
 
   handleSignOut = () => {
-    this.setState({
-      authenticated: false
-    });
+    this.props.logout();
     this.props.history.push('/');
   };
 
   render() {
-    const { authenticated } = this.state;
+    const { auth } = this.props;
+    const authenticated = auth.authenticated;
     return (
       <Menu inverted fixed="top">
         <Container>
@@ -58,7 +59,10 @@ class NavBar extends Component {
             </Menu.Item>
           )}
           {authenticated ? (
-            <SignedInMenu signOut={this.handleSignOut} />
+            <SignedInMenu
+              currentUser={auth.currentUser}
+              signOut={this.handleSignOut}
+            />
           ) : (
             <SignedOutMenu
               signIn={this.handleSignIn}
@@ -73,7 +77,7 @@ class NavBar extends Component {
 
 export default withRouter(
   connect(
-    null,
+    mapState,
     actions
   )(NavBar)
 );
