d7b2dafeeb05f8fbc3f27a73c8b0a604a647a1de
Devin <devdhera@gmail.com>
2019-05-03 23:15:02 +0530

    Added Firebase Auth

--- src/features/auth/authActions.jsx
+++ src/features/auth/authActions.jsx
@@ -1,10 +1,17 @@
-import { LOGIN_USER, SIGN_OUT_USER } from './authConstants';
+import { SIGN_OUT_USER } from './authConstants';
 import { closeModal } from '../modals/modalActions';
 
 export const login = creds => {
-  return dispatch => {
-    dispatch({ type: LOGIN_USER, payload: { creds } });
-    dispatch(closeModal());
+  return async (dispatch, getState, { getFirebase }) => {
+    const firebase = getFirebase();
+    try {
+      await firebase
+        .auth()
+        .signInWithEmailAndPassword(creds.email, creds.password);
+      dispatch(closeModal());
+    } catch (error) {
+      console.log(error);
+    }
   };
 };
 
