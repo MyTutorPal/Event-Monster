94f4ddf0ab5403bfb292d1ee5d4c7fc52e4d81f6
Devin <devdhera@gmail.com>
2019-05-04 20:00:20 +0530

    Added react dropzone configurations

--- src/features/user/Settings/PhotosPage.jsx
+++ src/features/user/Settings/PhotosPage.jsx
@@ -6,10 +6,24 @@ import {
   Divider,
   Grid,
   Button,
-  Card
+  Card,
+  Icon
 } from 'semantic-ui-react';
+import Dropzone from 'react-dropzone';
 
 class PhotosPage extends Component {
+  state = {
+    files: [],
+    fileName: ''
+  };
+
+  onDrop = files => {
+    this.setState({
+      files,
+      fileName: files[0].name
+    });
+  };
+
   render() {
     return (
       <Segment>
@@ -18,6 +32,12 @@ class PhotosPage extends Component {
           <Grid.Row />
           <Grid.Column width={4}>
             <Header color="teal" sub content="Step 1 - Add Photo" />
+            <Dropzone onDrop={this.onDrop} multiple={false}>
+              <div style={{ paddingTop: '30px', textAlign: 'center' }}>
+                <Icon name="upload" size="huge" />
+                <Header content="Drop image here or click to add" />
+              </div>
+            </Dropzone>
           </Grid.Column>
           <Grid.Column width={1} />
           <Grid.Column width={4}>
@@ -26,6 +46,12 @@ class PhotosPage extends Component {
           <Grid.Column width={1} />
           <Grid.Column width={4}>
             <Header sub color="teal" content="Step 3 - Preview and Upload" />
+            {this.state.files[0] && (
+              <Image
+                style={{ minHeight: '200px', minWidth: '200px' }}
+                src={this.state.files[0].preview}
+              />
+            )}
           </Grid.Column>
         </Grid>
 
