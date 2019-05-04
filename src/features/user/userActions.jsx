0526cd1426542751e929c8ff27ada94b06d0ba82
Devin <devdhera@gmail.com>
2019-05-04 20:40:16 +0530

    Configured photo upload method

--- src/features/user/userActions.jsx
+++ src/features/user/userActions.jsx
@@ -19,3 +19,49 @@ export const updateProfile = user => async (
     console.log(error);
   }
 };
+
+export const uploadProfileImage = (file, fileName) => async (
+  dispatch,
+  getState,
+  { getFirebase, getFirestore }
+) => {
+  const firebase = getFirebase();
+  const firestore = getFirestore();
+  const user = firebase.auth().currentUser;
+  const path = `${user.uid}/user_images`;
+  const options = {
+    name: fileName
+  };
+  try {
+    // upload the file to firebase storage
+    let uploadedFile = await firebase.uploadFile(path, file, null, options);
+    // get url of image
+    let downloadURL = await uploadedFile.uploadTaskSnapshot.downloadURL;
+    // get userdoc
+    let userDoc = await firestore.get(`users/${user.uid}`);
+    // check if user has photo, if not update the profile with the new image
+    if (!userDoc.data().photoURL) {
+      await firebase.updateProfile({
+        photoURL: downloadURL
+      });
+      await user.updateProfile({
+        photoURL: downloadURL
+      });
+    }
+    // add the new photo to the photos collection
+    return await firestore.add(
+      {
+        collection: 'users',
+        doc: user.uid,
+        subcollections: [{ collection: 'photos' }]
+      },
+      {
+        name: fileName,
+        url: downloadURL
+      }
+    );
+  } catch (error) {
+    console.log(error);
+    throw new Error('Problem uploading photo');
+  }
+};
