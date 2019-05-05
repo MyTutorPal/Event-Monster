6ee202b0d6ebae99ab57eb281705c405c045583b
Devin <devdhera@gmail.com>
2019-05-05 12:15:05 +0530

    Added object mapping helper

--- src/app/common/util/helpers.js
+++ src/app/common/util/helpers.js
@@ -1,5 +1,13 @@
 import moment from 'moment';
 
+export const objectToArray = object => {
+  if (object) {
+    return Object.entries(object).map(e =>
+      Object.assign({}, e[1], { id: e[0] })
+    );
+  }
+};
+
 export const createNewEvent = (user, photoURL, event) => {
   event.date = moment(event.date).toDate();
   return {
