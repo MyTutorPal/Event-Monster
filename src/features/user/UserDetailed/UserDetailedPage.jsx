6f562a9839fd237a760d8e5a8d74a9ca626e2db9
Devin <devdhera@gmail.com>
2019-05-05 19:38:28 +0530

    Fixed profile viewing issue

--- src/features/user/UserDetailed/UserDetailedPage.jsx
+++ src/features/user/UserDetailed/UserDetailedPage.jsx
@@ -1,30 +1,35 @@
 import React, { Component } from 'react';
 import { Grid } from 'semantic-ui-react';
 import { connect } from 'react-redux';
-import { firestoreConnect } from 'react-redux-firebase';
+import { firestoreConnect, isEmpty } from 'react-redux-firebase';
 import { compose } from 'redux';
 import UserDetailedDescription from './UserDetailedDescription';
 import UserDetailedEvents from './UserDetailedEvents';
 import UserDetailedHeader from './UserDetailedHeader';
 import UserDetailedPhotos from './UserDetailedPhotos';
 import UserDetailedSidebar from './UserDetailedSidebar';
+import { userDetailedQuery } from '../userQueries';
 
-const query = ({ auth }) => {
-  return [
-    {
-      collection: 'users',
-      doc: auth.uid,
-      subcollections: [{ collection: 'photos' }],
-      storeAs: 'photos'
-    }
-  ];
+const mapState = (state, ownProps) => {
+  let userUid = null;
+  let profile = {};
+
+  if (ownProps.match.params.id === state.auth.uid) {
+    profile = state.firebase.profile;
+  } else {
+    profile =
+      !isEmpty(state.firestore.ordered.profile) &&
+      state.firestore.ordered.profile[0];
+    userUid = ownProps.match.params.id;
+  }
+
+  return {
+    profile,
+    userUid,
+    auth: state.firebase.auth,
+    photos: state.firestore.ordered.photos
+  };
 };
-
-const mapState = state => ({
-  profile: state.firebase.profile,
-  auth: state.firebase.auth,
-  photos: state.firestore.ordered.photos
-});
 
 class UserDetailedPage extends Component {
   render() {
@@ -43,5 +48,5 @@ class UserDetailedPage extends Component {
 
 export default compose(
   connect(mapState),
-  firestoreConnect(auth => query(auth))
+  firestoreConnect((auth, userUid) => userDetailedQuery(auth, userUid))
 )(UserDetailedPage);
