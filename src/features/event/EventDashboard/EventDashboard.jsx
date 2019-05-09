aa69b5c66cb12cea34d88528c919e89e6c9b4fa8
Devin <devdhera@gmail.com>
2019-05-09 10:07:44 +0530

    Improved the UX of paging

--- src/features/event/EventDashboard/EventDashboard.jsx
+++ src/features/event/EventDashboard/EventDashboard.jsx
@@ -17,7 +17,9 @@ const actions = {
 };
 class EventDashboard extends Component {
   state = {
-    moreEvents: false
+    moreEvents: false,
+    loadingInitial: true,
+    loadedEvents: []
   };
   async componentDidMount() {
     let next = await this.props.getEventsForDashboard();
@@ -25,7 +27,16 @@ class EventDashboard extends Component {
 
     if (next && next.docs && next.docs.length > 1) {
       this.setState({
-        moreEvents: true
+        moreEvents: true,
+        loadingInitial: false
+      });
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.events !== nextProps.events) {
+      this.setState({
+        loadedEvents: [...this.state.loadedEvents, ...nextProps.events]
       });
     }
   }
@@ -48,13 +59,17 @@ class EventDashboard extends Component {
   };
 
   render() {
-    const { events, loading } = this.props;
-    if (loading) return <LoadingComponent inverted={true} />;
+    const { loading } = this.props;
+    if (this.state.loadingInitial) return <LoadingComponent inverted={true} />;
     return (
       <Grid>
         <Grid.Column width={10}>
-          <EventList deleteEvent={this.handleDeleteEvent} events={events} />
+          <EventList
+            deleteEvent={this.handleDeleteEvent}
+            events={this.state.loadedEvents}
+          />
           <Button
+            loading={loading}
             onClick={this.getNextEvents}
             disabled={!this.state.moreEvents}
             content="More"
