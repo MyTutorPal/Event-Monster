6ee0e47b91c80de73d3b2cbe5c538934eeaeb24b
Devin <devdhera@gmail.com>
2019-05-09 10:24:01 +0530

    Configured Infinite Scrolling

--- src/features/event/EventDashboard/EventDashboard.jsx
+++ src/features/event/EventDashboard/EventDashboard.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { firestoreConnect } from 'react-redux-firebase';
-import { Grid, Button } from 'semantic-ui-react';
+import { Grid, Loader } from 'semantic-ui-react';
 import EventList from '../EventList/EventList';
 import { getEventsForDashboard } from '../eventActions';
 import LoadingComponent from '../../../app/layout/LoadingComponent';
@@ -54,31 +54,25 @@ class EventDashboard extends Component {
     }
   };
 
-  handleDeleteEvent = eventId => () => {
-    this.props.deleteEvent(eventId);
-  };
-
   render() {
     const { loading } = this.props;
+    const { moreEvents, loadedEvents } = this.state;
     if (this.state.loadingInitial) return <LoadingComponent inverted={true} />;
     return (
       <Grid>
         <Grid.Column width={10}>
           <EventList
-            deleteEvent={this.handleDeleteEvent}
-            events={this.state.loadedEvents}
-          />
-          <Button
             loading={loading}
-            onClick={this.getNextEvents}
-            disabled={!this.state.moreEvents}
-            content="More"
-            color="green"
-            floated="right"
+            moreEvents={moreEvents}
+            events={loadedEvents}
+            getNextEvents={this.getNextEvents}
           />
         </Grid.Column>
         <Grid.Column width={6}>
           <EventActivity />
+        </Grid.Column>
+        <Grid.Column width={10}>
+          <Loader active={loading} />
         </Grid.Column>
       </Grid>
     );
