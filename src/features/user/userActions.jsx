e3715701678b515ea45c2673c1cb104c1b5e0e02
Devin <devdhera@gmail.com>
2019-05-12 20:53:22 +0530

    Configured followUser action

--- src/features/user/userActions.jsx
+++ src/features/user/userActions.jsx
@@ -219,3 +219,29 @@ export const getUserEvents = (userUid, activeTab) => async (
     dispatch(asyncActionError());
   }
 };
+
+export const followUser = userToFollow => async (
+  dispatch,
+  getState,
+  { getFirestore }
+) => {
+  const firestore = getFirestore();
+  const user = firestore.auth().currentUser;
+  const following = {
+    photoURL: userToFollow.photoURL || '/assets/user.png',
+    city: userToFollow.city || 'Unknown city',
+    displayName: userToFollow.displayName
+  };
+  try {
+    await firestore.set(
+      {
+        collection: 'users',
+        doc: user.uid,
+        subcollections: [{ collection: 'following', doc: userToFollow.id }]
+      },
+      following
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
