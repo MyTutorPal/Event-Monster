5436cfc566c4fb0fc123fa63852e7057be62f8c8
Devin <devdhera@gmail.com>
2019-05-12 21:16:29 +0530

    Populating PeopleDashboard with Data

--- src/features/user/PeopleDashboard/PersonCard.jsx
+++ src/features/user/PeopleDashboard/PersonCard.jsx
@@ -4,13 +4,13 @@ import { Link } from 'react-router-dom';
 
 const PersonCard = ({ user }) => {
   return (
-    <Card as={Link} to="/profile/12">
-      <Image src="/assets/user.png" />
+    <Card as={Link} to={`/profile/${user.id}`}>
+      <Image src={user.photoURL} />
       <Card.Content textAlign="center">
-        <Card.Header content="Display Name" />
+        <Card.Header content={user.displayName} />
       </Card.Content>
       <Card.Meta textAlign="center">
-        <span>City</span>
+        <span>{user.city}</span>
       </Card.Meta>
     </Card>
   );
