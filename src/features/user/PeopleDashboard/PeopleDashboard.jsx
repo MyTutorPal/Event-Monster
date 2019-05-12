20b712c1b4a2feed99aa697c893da9747fcf3767
Devin <devdhera@gmail.com>
2019-05-12 20:44:37 +0530

    Customized PeopleDashboard

--- src/features/user/PeopleDashboard/PeopleDashboard.jsx
+++ src/features/user/PeopleDashboard/PeopleDashboard.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
+import { Grid, Segment, Header, Card } from 'semantic-ui-react';
+import PersonCard from './PersonCard';
 
 const PeopleDashboard = () => {
   return (
-    <div>
-      <h1>People Dashboard</h1>
-    </div>
+    <Grid>
+      <Grid.Column width={16}>
+        <Segment>
+          <Header dividing content="People following me" />
+          <Card.Group itemsPerRow={8} stackable>
+            <PersonCard />
+            <PersonCard />
+            <PersonCard />
+            <PersonCard />
+            <PersonCard />
+          </Card.Group>
+        </Segment>
+        <Segment>
+          <Header dividing content="People I'm following" />
+          <Card.Group itemsPerRow={8} stackable>
+            <PersonCard />
+            <PersonCard />
+            <PersonCard />
+          </Card.Group>
+        </Segment>
+      </Grid.Column>
+    </Grid>
   );
 };
 
