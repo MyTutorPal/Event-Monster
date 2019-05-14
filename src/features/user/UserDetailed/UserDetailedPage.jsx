5fbb55aa2bf928529f2887010195ff80c05ff2eb
Devin <devdhera@gmail.com>
2019-05-14 13:27:16 +0530

    Fixed not found users issue

--- src/features/user/UserDetailed/UserDetailedPage.jsx
+++ src/features/user/UserDetailed/UserDetailedPage.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { firestoreConnect, isEmpty } from 'react-redux-firebase';
 import { compose } from 'redux';
+import { toastr } from 'react-redux-toastr';
 import { Grid } from 'semantic-ui-react';
 import UserDetailedDescription from './UserDetailedDescription';
 import UserDetailedEvents from './UserDetailedEvents';
@@ -49,6 +50,13 @@ const actions = {
 
 class UserDetailedPage extends Component {
   async componentDidMount() {
+    let user = await this.props.firestore.get(
+      `users/${this.props.match.params.id}`
+    );
+    if (!user.exists) {
+      toastr.error('Not Found', 'This is not the user you are looking for');
+      this.props.history.push('/error');
+    }
     await this.props.getUserEvents(this.props.userUid);
   }
 
@@ -70,7 +78,7 @@ class UserDetailedPage extends Component {
       unfollowUser
     } = this.props;
     const isCurrentUser = auth.uid === match.params.id;
-    const loading = Object.values(requesting).some(a => a === true);
+    const loading = requesting[`users/${match.params.id}`];
     const isFollowing = !isEmpty(following);
 
     if (loading) return <LoadingComponent inverted={true} />;
