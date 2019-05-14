caa2a12d17f89ba2942189ec6c4f1d11e9a6ec7e
Devin <devdhera@gmail.com>
2019-05-14 14:37:44 +0530

    Removed boilerplates

--- src/features/home/HomePage.jsx
+++ src/features/home/HomePage.jsx
@@ -23,23 +23,6 @@ const HomePage = ({ history }) => {
           </div>
         </div>
       </div>
-      <div style={{ textAlign: 'center' }}>
-        Icons made by{' '}
-        <a href="http://www.freepik.com" title="Freepik">
-          Freepik
-        </a>{' '}
-        from{' '}
-        <a href="https://www.flaticon.com/" title="Flaticon">
-          www.flaticon.com
-        </a>{' '}
-        is licensed by{' '}
-        <a
-          href="http://creativecommons.org/licenses/by/3.0/"
-          title="Creative Commons BY 3.0"
-        >
-          CC 3.0 BY
-        </a>
-      </div>
     </div>
   );
 };
